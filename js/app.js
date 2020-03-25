726283aebfeb81819d3d55a3f9bd360f09a30601
Nomanishaq <nomanishaq251@gmail.com>
2020-03-25 22:35:15 +0500

    setting

--- js/app.js
+++ js/app.js
@@ -3,21 +3,22 @@ $(document).ready(function(){
 	    $("#myModal").modal('show'); }, 5000);
 });
 
+let content = (tasbeehValue,title,limits,Contet)=>{
+      tasbeehVal = tasbeehValue
+      contentTitle.innerHTML = title;
+      limit.innerHTML = limits;
+      mainContent.innerHTML = Contet;
+}
+
 let countfunc = ()=> {
       tasbeehVal += 1;
       tasbeeh.innerHTML = tasbeehVal;
       tasbeeh.style.color = "red";
       if (tasbeehVal === 3 && contentTitle.innerHTML == "Darood Pak") {
-            tasbeehVal = 0;
-            contentTitle.innerHTML = "Dua";
-            limit.innerHTML = "11";
-            mainContent.innerHTML = "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْبَرَصِ وَالْجُنُونِ وَالْجُذَامِ وَمِنْ سَيِّئِ الأَسْقَامِ";
+            content(0,'Dua','11','اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْبَرَصِ وَالْجُنُونِ وَالْجُذَامِ وَمِنْ سَيِّئِ الأَسْقَامِ');
       }
       else if (tasbeehVal === 11 && contentTitle.innerHTML == "Dua") {
-            tasbeehVal = 0;
-            contentTitle.innerHTML = "Ayat-e-Kareema";
-            limit.innerHTML = "100";
-            mainContent.innerHTML =" لاَّ إِلَـهَ إِلاَّ أَنتَ سُبْحَـنَكَ إِنِّى كُنتُ مِنَ الظَّـلِمِينَ";
+            content(0,'Ayat-e-Kareema','100',' لاَّ إِلَـهَ إِلاَّ أَنتَ سُبْحَـنَكَ إِنِّى كُنتُ مِنَ الظَّـلِمِينَ');
       }
       else if (tasbeehVal === 100 && contentTitle.innerHTML == "Ayat-e-Kareema") {
             $("#Modal").modal('show');
@@ -30,18 +31,13 @@ let countdisplay = ()=> {
 }
 
 let randomly = ()=>{
-      tasbeehVal += 1;
-      tasbeehVal = 0;
-      tasbeeh.innerHTML = tasbeehVal;
-      contentTitle.innerHTML = "Astaghfirullah";
-      tasbeeh.style.color = "red";
-      limit.innerHTML = "No Limit";
-      mainContent.innerHTML = "أَسْتَغْفِرُ اللّٰهَ";
+      content(0,'Astaghfirullah','No Limit','أَسْتَغْفِرُ اللّٰهَ');
 }
 
 let reset = ()=>{
       location.reload();
 }
+
 astagfar.addEventListener('click',randomly);
 resetbtn.addEventListener('click',reset);
 count.addEventListener("click",countfunc);
